a438cdd32c463cc8def3cc876c910eb6a46a9b56
SandraCGarcia <sandra.gcanete@gmail.com>
2018-08-02 09:00:15 +0200

    first commit

---
+++ src/Tarjeta.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+const titleElement = <h1 className="App-title">Querido React, No Me Odies...</h1>;
+
+class Tarjeta extends Component {
+  render() {
+    return (
+      <div className="App">
+
+      </div>
+    );
+  }
+}
+
+export default Tarjeta;
